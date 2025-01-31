20c04c7caa5ded99711eaec23ca130113209cf4c
Mahmoud <johndoe@example.commahmoud.alhayek@plan-b-gmbh.com>
2025-01-31 13:11:17 +0100

    cors

--- src/services/customer-number-server/index.js
+++ src/services/customer-number-server/index.js
@@ -1,24 +1,18 @@
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+
+const fastify = Fastify({ logger: true });
 
 fastify.register(cors, {
-  origin: "*", // السماح بجميع المصادر
+  origin: "*", 
 });
 
-// تعريف مسار الجذر
+
 fastify.get('/', async (request, reply) => {
   return { message: 'Welcome to the Customer Number System!' };
 });
 
-// تشغيل الخادم
-// const start = async () => {
-//   try {
-//     await fastify.listen({ port: 3000 });
-//     console.log("Server is running at http://localhost:3000/");
-//   } catch (err) {
-//     fastify.log.error(err);
-//     process.exit(1);
-//   }
-// };
-// start();
+
 const start = async () => {
   try {
     await fastify.listen({port: 3000});
@@ -27,3 +21,4 @@ const start = async () => {
     fastify.log.error(err);
     process.exit(1);
   }
+}
